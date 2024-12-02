e672816ee6469fa047d5daf702fbcc64907a9bc8
luna156 <63947262+luna156@users.noreply.github.com>
2024-12-02 11:02:28 +0900

    [Feat] club 로그인 및 로그아웃 추가

--- src/main/java/com/likelion/innerjoin/user/repository/ClubRepository.java
+++ src/main/java/com/likelion/innerjoin/user/repository/ClubRepository.java
@@ -4,4 +4,5 @@ import com.likelion.innerjoin.user.model.entity.Club;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClubRepository extends JpaRepository<Club, Long> {
+    Club findByLoginIdAndPassword(String loginId, String password);
 }
