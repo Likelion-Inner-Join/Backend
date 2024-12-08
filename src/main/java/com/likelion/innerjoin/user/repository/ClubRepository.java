d03cbf3135972cc2c906c1a4877c52b6ef992c9b
BlueRedOrange <jmg0916789@gmail.com>
2024-12-08 18:02:57 +0900

    [Feat] 동아리 이메일 중복확인 api추가

--- src/main/java/com/likelion/innerjoin/user/repository/ClubRepository.java
+++ src/main/java/com/likelion/innerjoin/user/repository/ClubRepository.java
@@ -3,6 +3,9 @@ package com.likelion.innerjoin.user.repository;
 import com.likelion.innerjoin.user.model.entity.Club;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClubRepository extends JpaRepository<Club, Long> {
     Club findByLoginIdAndPassword(String loginId, String password);
+    Optional<Club> findByEmail(String email);
 }
