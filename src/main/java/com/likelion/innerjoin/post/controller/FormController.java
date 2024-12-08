20f90a562213b6639dddf149321ce56d924fbace
luna156 <v010309@naver.com>
2024-12-08 16:47:13 +0900

    [Docs] swagger 추가

--- src/main/java/com/likelion/innerjoin/post/controller/FormController.java
+++ src/main/java/com/likelion/innerjoin/post/controller/FormController.java
@@ -5,6 +5,9 @@ import com.likelion.innerjoin.post.model.dto.request.FormRequestDto;
 import com.likelion.innerjoin.post.model.dto.response.FormListResponseDto;
 import com.likelion.innerjoin.post.model.dto.response.FormResponseDto;
 import com.likelion.innerjoin.post.service.FormService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,14 +23,22 @@ public class FormController {
     private final FormService formService;
 
     @PostMapping("")
+    @Operation(summary = "지원폼 생성 api")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적인 응답"),
+            @ApiResponse(responseCode = "401", description = "세션값이 잘못되었습니다.")
+    })
     public CommonResponse<FormResponseDto> createForm(@RequestBody FormRequestDto formRequestDto, HttpSession session) {
         return new CommonResponse<>(formService.createForm(formRequestDto, session));
     }
 
     @GetMapping("")
+    @Operation(summary = "지원폼 목록 조회 api")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적인 응답"),
+            @ApiResponse(responseCode = "401", description = "세션값이 잘못되었습니다.")
+    })
     public CommonResponse<List<FormListResponseDto>> getFormList(HttpSession session) {
         return new CommonResponse<>(formService.getFormList(session));
     }
-
-
 }
