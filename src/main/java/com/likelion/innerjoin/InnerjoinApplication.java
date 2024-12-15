a264db38be529a6101d939f12250524f91b451bd
luna156 <v010309@naver.com>
2024-12-15 19:17:42 +0900

    [Fix] CORS 해결

--- src/main/java/com/likelion/innerjoin/InnerjoinApplication.java
+++ src/main/java/com/likelion/innerjoin/InnerjoinApplication.java
@@ -2,7 +2,10 @@ package com.likelion.innerjoin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -12,4 +15,17 @@ public class InnerjoinApplication {
 		SpringApplication.run(InnerjoinApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:3000",
+								"https://innerjoin.vercel.app")  // 허용할 도메인
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메소드
+						.allowedHeaders("*"); // 허용할 헤더
+			}
+		};
+	}
 }
