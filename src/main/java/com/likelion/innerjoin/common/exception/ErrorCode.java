a88bcb931515d49949ef92bf94dafe5f7960304d
luna156 <63947262+luna156@users.noreply.github.com>
2024-12-21 16:28:29 +0900

    [Fix] conflict 해결

--- src/main/java/com/likelion/innerjoin/common/exception/ErrorCode.java
+++ src/main/java/com/likelion/innerjoin/common/exception/ErrorCode.java
@@ -36,12 +36,9 @@ public enum ErrorCode {
     JSON_CONVERT_ERROR(false, HttpStatus.INTERNAL_SERVER_ERROR.value(), "JSON 변환중 오류가 발생하였습니다."),
     CLUB_NOT_FOUND(false, 404, "동아리를 찾을 수 없습니다."),
     FORM_NOT_FOUND(false, 404, "지원폼을 찾을 수 없습니다."),
-    DUPLICATE_LOGIN_ID(false, HttpStatus.BAD_REQUEST.value(), "이미 존재하는 아이디입니다."),
     ALREADY_APPLIED(false, HttpStatus.BAD_REQUEST.value(), "이미 지원했습니다."),
     MEETING_TIME_NOT_FOUND(false, HttpStatus.NOT_FOUND.value(), "면접 시간을 찾을 수 없습니다."),
-    ALLOWED_NUM_EXCEEDED(false, HttpStatus.BAD_REQUEST.value(), "면접 허용 인원을 초과했습니다."),
-    ALREADY_APPLIED(false, HttpStatus.BAD_REQUEST.value(), "이미 지원했습니다.");
-
+    ALLOWED_NUM_EXCEEDED(false, HttpStatus.BAD_REQUEST.value(), "면접 허용 인원을 초과했습니다.");
 
     private final Boolean isSuccess;
     private final int code;
