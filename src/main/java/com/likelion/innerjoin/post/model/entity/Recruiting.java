8a9d020f91ca26268bc54bd2a4ccb050dc0adc17
jeongju <wnwjdqkr@gmail.com>
2024-12-23 19:05:12 +0900

    fix: recruiting 엔티티에 예약 시작/끝 일시 추가

--- src/main/java/com/likelion/innerjoin/post/model/entity/Recruiting.java
+++ src/main/java/com/likelion/innerjoin/post/model/entity/Recruiting.java
@@ -1,5 +1,6 @@
 package com.likelion.innerjoin.post.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.likelion.innerjoin.common.entity.DataEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -31,6 +33,14 @@ public class Recruiting extends DataEntity {
 
     @Column(name = "job_title")
     private String jobTitle;
+
+    @Column(name = "reservation_start_time")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime reservationStartTime;
+
+    @Column(name = "reservation_end_time")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime reservationEndTime;
 
     @OneToMany(mappedBy = "recruiting", orphanRemoval = true, cascade = CascadeType.ALL)
     private List<Application> application;
