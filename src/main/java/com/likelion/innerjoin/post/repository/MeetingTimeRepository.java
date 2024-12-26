fef3735e602293fd5977d474de7be8e106584883
luna156 <v010309@naver.com>
2024-12-26 11:10:09 +0900

    [Fix] 지원자 정보 수정시 meeting time 설정로직 변경
    면접시작 및 종료시간으로 조회하여 설정

--- src/main/java/com/likelion/innerjoin/post/repository/MeetingTimeRepository.java
+++ src/main/java/com/likelion/innerjoin/post/repository/MeetingTimeRepository.java
@@ -4,9 +4,11 @@ import com.likelion.innerjoin.post.model.entity.MeetingTime;
 import com.likelion.innerjoin.post.model.entity.Recruiting;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MeetingTimeRepository extends JpaRepository<MeetingTime, Long> {
     List<MeetingTime> findByRecruiting(Recruiting recruiting);
     List<MeetingTime> findByRecruitingId(Long recruitingId);
+    MeetingTime findByMeetingStartTimeAndMeetingEndTimeAndRecruiting(LocalDateTime meetingStartTime, LocalDateTime meetingEndTime, Recruiting recruiting);
 }
