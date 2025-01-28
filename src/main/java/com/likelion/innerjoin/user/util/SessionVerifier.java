33b9b9286658994e857ef9c063d69e9e5156ff25
luna156 <63947262+luna156@users.noreply.github.com>
2025-01-28 17:14:00 +0900

    [Feat] SessionVerifier에 club과 applicant 가져오는 메서드 추가

--- src/main/java/com/likelion/innerjoin/user/util/SessionVerifier.java
+++ src/main/java/com/likelion/innerjoin/user/util/SessionVerifier.java
@@ -1,6 +1,8 @@
 package com.likelion.innerjoin.user.util;
 
 import com.likelion.innerjoin.post.exception.UnauthorizedException;
+import com.likelion.innerjoin.user.model.entity.Applicant;
+import com.likelion.innerjoin.user.model.entity.Club;
 import com.likelion.innerjoin.user.model.entity.User;
 import com.likelion.innerjoin.user.repository.ApplicantRepository;
 import com.likelion.innerjoin.user.repository.ClubRepository;
@@ -14,6 +16,7 @@ public class SessionVerifier {
     private final ClubRepository clubRepository;
     private final ApplicantRepository applicantRepository;
 
+    @Deprecated
     public User verifySession(HttpSession session) {
         if( session == null ){
             throw new UnauthorizedException("잘못된 접근입니다.");
@@ -35,6 +38,54 @@ public class SessionVerifier {
             return clubRepository.findById(userId).orElseThrow(() -> new UnauthorizedException("잘못된 유저입니다."));
         }else if ( role.equals("applicant") ){
             return applicantRepository.findById(userId).orElseThrow(() -> new UnauthorizedException("잘못된 유저입니다."));
+        }else {
+            throw new UnauthorizedException("잘못된 유저입니다.");
+        }
+    }
+
+    public Applicant getApplicant(HttpSession session) {
+        if( session == null ){
+            throw new UnauthorizedException("잘못된 접근입니다.");
+        }
+
+        if( session.getAttribute("userId") == null || session.getAttribute("role") == null ){
+            throw new UnauthorizedException("잘못된 접근입니다.");
+        }
+
+        Long userId = (Long) session.getAttribute("userId");
+
+        String role = (String) session.getAttribute("role");
+
+        if( userId == null ){
+            throw new UnauthorizedException("잘못된 유저입니다.");
+        }
+
+        if ( role.equals("applicant") ){
+            return applicantRepository.findById(userId).orElseThrow(() -> new UnauthorizedException("잘못된 유저입니다."));
+        }else {
+            throw new UnauthorizedException("잘못된 유저입니다.");
+        }
+    }
+
+    public Club getClub(HttpSession session) {
+        if( session == null ){
+            throw new UnauthorizedException("잘못된 접근입니다.");
+        }
+
+        if( session.getAttribute("userId") == null || session.getAttribute("role") == null ){
+            throw new UnauthorizedException("잘못된 접근입니다.");
+        }
+
+        Long userId = (Long) session.getAttribute("userId");
+
+        String role = (String) session.getAttribute("role");
+
+        if( userId == null ){
+            throw new UnauthorizedException("잘못된 유저입니다.");
+        }
+
+        if( role.equals("club") ){
+            return clubRepository.findById(userId).orElseThrow(() -> new UnauthorizedException("잘못된 유저입니다."));
         }else {
             throw new UnauthorizedException("잘못된 유저입니다.");
         }
